253d341c5c6f57eb9bb96a2017c4acbc549bb220
ariyapour <aref.ariyapour@gmail.com>
2025-10-19 15:11:25 +0200

    Make separate functions for similarity check and getting the results by url.

--- src/clip_search.py
+++ src/clip_search.py
@@ -1,34 +1,47 @@
 from utils import load_split_model, fetch_image
-import torch
-import open_clip
+from typing import List, Tuple
 import numpy as np
-import io
-import os
+import open_clip
+import torch
 
 
 class CLIPSearch:
-    def __init__(self, model_path="models", prefix="clip_model_part_",
-                 device="cuda" if torch.cuda.is_available() else "cpu"):
+    def __init__(self, model_path: str = "models", prefix: str = "clip_model_part_",
+                 device: str = "cuda" if torch.cuda.is_available() else "cpu"):
+        """Initialize CLIP model and tokenizer"""
         self.device = device
         self.preprocess, self.model = load_split_model()
         self.tokenizer = open_clip.get_tokenizer('ViT-B-32')
         self.model.to(device)
         self.model.eval()
 
-    def search_images_by_url(self, query,
-                             image_embedding_path="embeddings/image_coco_validation_2014_embeddings.npy",
-                             file_urls="embeddings/image_coco_validation_2014_coco_urls.npy",
-                             top_k=5):
-        # Load embeddings and filenames
+    def search_images(self, query: str,
+                      image_embedding_path: str = "embeddings/image_coco_validation_2014_embeddings.npy",
+                      top_k: int = 5):
+        """Return top-k similar image indices and their similarity scores"""
         image_embeddings = np.load(image_embedding_path)
-        file_urls = np.load(file_urls)
         text_tokens = self.tokenizer([query]).to(self.device)
         with torch.no_grad():
             text_features = self.model.encode_text(text_tokens)
             text_features /= text_features.norm(dim=-1, keepdim=True)
             similarity = (text_features.cpu().numpy() @ image_embeddings.T)[0]
             top_indices = similarity.argsort()[-top_k:][::-1]
-            results = [(file_urls[i], similarity[i]) for i in top_indices]
-            
-        urls = [url for url, _ in results]
-        return [fetch_image(url) for url in urls], urls  # images, urls_state
+            results = [(i, similarity[i]) for i in top_indices]
+
+        return results
+
+    def get_image_urls(self, top_k_indices: List[Tuple[int, float]],
+                       file_urls: str = "embeddings/image_coco_validation_2014_coco_urls.npy"):
+        """Get image URLs from indices"""
+        file_urls = np.load(file_urls)
+        return [(file_urls[index]) for index, similarity in top_k_indices]
+
+    def search_images_by_url(self, query: str,
+                             image_embedding_path: str = "embeddings/image_coco_validation_2014_embeddings.npy",
+                             file_urls: str = "embeddings/image_coco_validation_2014_coco_urls.npy",
+                             top_k: int = 5):
+        """Return list of PIL Images from URLs"""
+        top_k_indices = self.search_images(query, image_embedding_path, top_k)
+        urls = self.get_image_urls(top_k_indices, file_urls)
+        images = [fetch_image(url) for url in urls]
+        return images, urls
